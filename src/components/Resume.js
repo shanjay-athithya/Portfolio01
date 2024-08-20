2dde2af25988b0e8dd89ec9415788562b306a563
shanjay-athithya <shanjayathithya2004@gmail.com>
2024-08-20 11:11:24 +0530

    resume update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,7 +7,7 @@ const Resume = () => {
         <h2 className="text-4xl text-cyan-400 font-bold mb-8">Resume</h2>
         <p className="text-lg mb-8">Download my resume to learn more about my professional experience and skills.</p>
         <a
-          href="/resume/shanjay_athithya.pdf"
+          href="/resume/shanjay_athithya_04.pdf"
           download
           className="bg-white text-black px-6 py-3 rounded-full font-semibold hover:bg-gray-200 transition duration-300"
         >
